badb4fb7d67f2cee3e9efa47a091e38786967ad4
YuanC <jerrych.97@gmail.com>
2019-12-02 19:07:27 -0500

    init commit

---
+++ Assets/Editor/GridObjectBuilderEditor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(GridObjectBuilder))]
+public class GridObjectBuilderEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        GridObjectBuilder gObjBuilder = (GridObjectBuilder)target;
+        DrawDefaultInspector();
+        
+        if (GUILayout.Button("BuildObject"))
+        {
+            gObjBuilder.BuildObject();
+        }
+    }
+}
---
+++ Assets/Scripts/GridObjectBuilder.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridObjectBuilder : MonoBehaviour
+{
+    public List<GameObject> PrefabList;
+    public int x = 0;
+    public int y = 0;
+    private int ListIndex = 0;
+
+    public void BuildObject()
+    {
+        Instantiate(PrefabList[ListIndex], new Vector3(0, 0, 0), Quaternion.identity);
+    }
+}   
